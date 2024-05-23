ff217a059f82b6038125cc29974cadc16524bff2
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2024-05-24 00:12:20 +0900

    選択肢をç整理

--- src/NormalData.tsx
+++ src/NormalData.tsx
@@ -8,7 +8,7 @@ export const NormalData = () => {
     const initialOptions = {
         first: ['肉', '野菜', '果物'],
         second: {肉: ['牛', '豚', '鶏'], 野菜: ['ジャガイモ', '人参', '玉ねぎ'], 果物: ['リンゴ', 'ミカン', 'バナナ']},
-        third: {牛: ['和牛', '洋牛'], 豚: ['国産', '外国産'], 鶏: ['むね肉', 'ひき肉'], ジャガイモ: ['男爵', 'メークイン'], 人参: ['国産', '外国産'], 玉ねぎ: ['国産', '外国産'], リンゴ: ['国産', '外国産'], ミカン: ['国産', '外国産'], バナナ: ['国産', '外国産']}
+        third: {牛: ['米沢牛', '松坂牛', '飛騨牛'], 豚: ['黒豚', 'アグー豚', '三元豚'], 鶏: ['比内地鶏', '名古屋コーチン', '軍鶏'], ジャガイモ: ['男爵', 'メークイン'], 人参: ['人参', '金時人参', '高麗人参'], 玉ねぎ: ['国産', '外国産'], リンゴ: ['フジ', '秋映'], ミカン: ['文旦', 'デコポン', '温州ミカン'], バナナ: ['台湾産', 'フィリピン産']}
     }
 
     const {
@@ -26,15 +26,15 @@ export const NormalData = () => {
     return (
         <Box display="flex" justifyContent="space-between" gap={5}>
             <Dropdown labelID="first-select-label"
-                      label="First" value={firstSelection} options={firstOptions} onChange={handleFirstSelection}
+                      label="食材" value={firstSelection} options={firstOptions} onChange={handleFirstSelection}
             />
             <Dropdown labelID="second-select-label"
-                      label="Second" value={secondSelection} options={secondOptions}
+                      label="材料" value={secondSelection} options={secondOptions}
                       onChange={handleSecondSelection}
 
             />
             <Dropdown labelID="third-select-label"
-                      label="Third" value={thirdSelection}
+                      label="詳細" value={thirdSelection}
                       options={thirdOptions} onChange={handleThirdSelection}
             />
         </Box>

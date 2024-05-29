75cf6bda71c59eeee3b87f2e4d0cd4a9c42522c5
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2024-05-29 21:44:45 +0900

    お題を変更

--- src/FlatItemDropdown.tsx
+++ src/FlatItemDropdown.tsx
@@ -3,37 +3,37 @@ import {FlatDataSelector} from "./FlatDataSelector.tsx";
 import {ItemType, useFlatItemSelection} from "./useFlatItemSelection.tsx";
 
 
-const deviceItem: ItemType[] = [
-    {itemId: "1", name: "USBメモリ", subItemId: "1", subItem: "Buffalo_1"},
-    {itemId: "1", name: "USBメモリ", subItemId: "2", subItem: "Buffalo_2"},
-    {itemId: "1", name: "USBメモリ", subItemId: "3", subItem: "Buffalo_3"},
-    {itemId: "2", name: "HDD", subItemId: "4", subItem: "IO_DATA_1"},
-    {itemId: "2", name: "HDD", subItemId: "5", subItem: "IO_DATA_2"},
-    {itemId: "2", name: "HDD", subItemId: "6", subItem: "IO_DATA_3"},
-    {itemId: "3", name: "SSD", subItemId: "7", subItem: "ELECOM_1"},
-    {itemId: "3", name: "SSD", subItemId: "8", subItem: "ELECOM_2"},
-    {itemId: "3", name: "SSD", subItemId: "9", subItem: "ELECOM_3"}
+const foodItems: ItemType[] = [
+    {itemId: "1", name: "野菜", subItemId: "1", subItem: "じゃがいも"},
+    {itemId: "1", name: "野菜", subItemId: "2", subItem: "ニンジン"},
+    {itemId: "1", name: "野菜", subItemId: "3", subItem: "玉ねぎ"},
+    {itemId: "2", name: "肉", subItemId: "4", subItem: "牛肉"},
+    {itemId: "2", name: "肉", subItemId: "5", subItem: "豚肉"},
+    {itemId: "2", name: "肉", subItemId: "6", subItem: "鶏肉"},
+    {itemId: "3", name: "果物", subItemId: "7", subItem: "りんご"},
+    {itemId: "3", name: "果物", subItemId: "8", subItem: "みかん"},
+    {itemId: "3", name: "果物", subItemId: "9", subItem: "いちご"}
 ];
 
 export const FlatItemDropdown = () => {
     const {
-        selectedItem: selectedDevice,
-        filteredSubItems: products,
-        selectedSubItem: selectedProduct,
-        handleItemChange: handleDeviceChange,
-        handleSubItemChange: handleProductChange,
-        uniqueItems: uniqueDevices
-    } = useFlatItemSelection(deviceItem);
+        selectedItem: selectedFoodCategory,
+        filteredSubItems: foods,
+        selectedSubItem: selectedFood,
+        handleItemChange: handleCategoryChange,
+        handleSubItemChange: handleFoodStuffChange,
+        uniqueItems: uniqueCategory
+    } = useFlatItemSelection(foodItems);
 
     return (
         <Box display="flex" gap={2} flexWrap="wrap">
-            <FlatDataSelector label="デバイス" labelId="device-select-label" selectedValue={selectedDevice}
-                              nominationsList={uniqueDevices}
-                              onChange={handleDeviceChange}/>
+            <FlatDataSelector label="カテゴリ" labelId="device-select-label" selectedValue={selectedFoodCategory}
+                              nominationsList={uniqueCategory}
+                              onChange={handleCategoryChange}/>
 
-            <FlatDataSelector label="製品" labelId="product-select-label" selectedValue={selectedProduct}
-                              nominationsList={products}
-                              onChange={handleProductChange}/>
+            <FlatDataSelector label="食材" labelId="product-select-label" selectedValue={selectedFood}
+                              nominationsList={foods}
+                              onChange={handleFoodStuffChange}/>
         </Box>
     );
 };
